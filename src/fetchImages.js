1b474729b4d086f2ef81ad6879a954be63327642
Nataliia Tatsiienko <natasha51star29@gmail.com>
2022-08-20 09:30:47 +0300

    add js

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,15 +1,20 @@
-// import Notiflix from 'notiflix';
-const BASE_URL = 'https://restcountries.com/v3.1';
-const fields = 'name,capital,population,flags,languages'
-function fetchImages(query) {
+import axios from "axios";
+import Notiflix from "notiflix";
 
-    // return fetch(`${BASE_URL}/name/${name}?fields=${fields}`).then(response => {
-    //     if (!response.ok) {
-    //         throw new Error(Notiflix.Notify.failure("Oops, there is no country with that name"));
-    //     }
-    //     return response.json();
-    // }
-    // );   
+
+const BASE_URL = 'https://pixabay.com/api/';
+
+
+async function fetchImages(query, page) {
+    console.log({ query, page });
+    const parameters = `?key=29384510-d3fd293ec4cac87d919319a0f&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+    return await axios.get(`${BASE_URL}${parameters}`)
+        .then(response => {
+            if (response.data.totalHits === 0) {
+                Notiflix.Notify.info(`Sorry, there are no images matching your search query. Please try again.`);
+            }
+            return response.data;
+        });
 }
 
-export default {fetchImages};
+export default { fetchImages };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,96 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import API from './fetchImages';
 
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    imageContainer: document.querySelector('.gallery'),
+    loadMore: document.querySelector('.load-more'),
+}
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMore.addEventListener('click', onLoadMore);
+
+let searchQuery = '';
+// let currentPage = 1;
+
+async function onSearch(event) {
+  event.preventDefault();
+
+  searchQuery = event.currentTarget.elements.searchQuery.value;
+
+  currentPage = 1;
+
+  if (searchQuery === '') {
+    clearImagesContainer();
+    refs.loadMore.classList.add('is-hidden');
+    return Notiflix.Notify.info(`Sorry, there are no images matching your search query. Please try again.`);
+  }
+  
+  try {
+    const images = await API.fetchImages(searchQuery, currentPage);
+
+    if (images.totalHits > 40) {
+      refs.loadMore.classList.remove('is-hidden');
+    } else {
+      refs.loadMore.classList.add('is-hidden');
+    };
+
+    if (images.totalHits > 0) {
+      Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`);
+
+      clearImagesContainer();
+
+      renderImagesCard(images.hits);
+
+      const { height: cardHeight } = refs.imageContainer.firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+      top: cardHeight * -100,
+      behavior: "smooth",});
+      
+    } 
+
+  } catch (error) { console.log(error) };
+  
+  }
+
+async function onLoadMore() {
+  currentPage += 1;
+
+  const images = await API.fetchImages(searchQuery, currentPage);
+
+  renderImagesCard(images.hits);
+}
+
+
+
+function renderImagesCard(images) {
+  const markup = images
+    .map(el => {
+        return `<div class="photo-card">
+  <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${el.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${el.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${el.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${el.downloads}
+    </p>
+  </div>
+</div>`
+    }).join('');
+  
+  refs.imageContainer.insertAdjacentHTML('beforeend', markup);
+};
+
+function clearImagesContainer() {
+  refs.imageContainer.innerHTML = '';
+}
